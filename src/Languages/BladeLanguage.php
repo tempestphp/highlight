0e645fbdb11d351aa7beff365db3487673f34e4f
Brent Roose <brent.roose@gmail.com>
2024-03-15 13:29:22 +0100

    wip

--- src/Languages/BladeLanguage.php
+++ src/Languages/BladeLanguage.php
@@ -2,12 +2,27 @@
 
 namespace Tempest\Highlight\Languages;
 
+use Tempest\Highlight\Languages\Blade\Injections\BladeKeywordInjection;
+use Tempest\Highlight\Languages\Blade\Injections\BladePhpInjection;
+use Tempest\Highlight\Languages\Blade\Patterns\BladeKeywordPattern;
+use Tempest\Highlight\Languages\Html\HtmlLanguage;
+
 class BladeLanguage extends HtmlLanguage
 {
+    public function getInjections(): array
+    {
+        return [
+            ...parent::getInjections(),
+            new BladePhpInjection(),
+            new BladeKeywordInjection(),
+        ];
+    }
+
     public function getPatterns(): array
     {
-        return parent::getPatterns() + [
-
-            ];
+        return [
+            ...parent::getPatterns(),
+            new BladeKeywordPattern(),
+        ];
     }
 }

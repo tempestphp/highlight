7900299c991622d68f143665b552d835638853b1
brendt <brent.roose@gmail.com>
2024-03-16 13:12:17 +0100

    Fix test

--- src/Languages/Php/Patterns/TypedConstantNamePattern.php
+++ src/Languages/Php/Patterns/TypedConstantNamePattern.php
@@ -10,7 +10,7 @@ use Tempest\Highlight\PatternTest;
 use Tempest\Highlight\Tokens\TokenType;
 
 #[PatternTest(
-    input: 'private const string BAR',
+    input: 'private const string BAR = ""',
     output: 'BAR',
 )]
 final readonly class TypedConstantNamePattern implements Pattern

948c74c23a7554e349c96db5c1d943f36ac155e2
Brent Roose <brent.roose@gmail.com>
2024-03-18 13:22:45 +0100

    Test

--- tests/Languages/Php/PhpLanguageTest.php
+++ tests/Languages/Php/PhpLanguageTest.php
@@ -27,6 +27,7 @@ class PhpLanguageTest extends TestCase
             ["'php()'", "'<span class=\"hl-value\">php()</span>'"],
             ["public const string|\Stringable MESSAGE = 'hi';", '<span class="hl-keyword">public</span> <span class="hl-keyword">const</span> <span class="hl-type">string|\Stringable</span> <span class="hl-property">MESSAGE</span> = \'<span class="hl-value">hi</span>\';'],
             ["public string|\Stringable \$message;", '<span class="hl-keyword">public</span> <span class="hl-type">string|\<span class="hl-type">Stringable</span></span> <span class="hl-property">$message</span>;'],
+            ['for($x = 0; $x < 150; $x++) {', '<span class="hl-keyword">f<span class="hl-keyword">or</span></span>($x = 0; $x &lt; 150; $x++) {']
         ];
     }
 }

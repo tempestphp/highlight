e5a27fd8e4c466ee1471b97f057b6eef64b3efde
brendt <brent.roose@gmail.com>
2024-03-20 05:59:42 +0100

    Test

--- tests/Languages/Php/PhpLanguageTest.php
+++ tests/Languages/Php/PhpLanguageTest.php
@@ -29,6 +29,7 @@ class PhpLanguageTest extends TestCase
             ["public string|\Stringable \$message;", '<span class="hl-keyword">public</span> <span class="hl-type">string|\<span class="hl-type">Stringable</span></span> <span class="hl-property">$message</span>;'],
             ['for($x = 0; $x < 150; $x++) {', '<span class="hl-keyword">for</span>($x = 0; $x &lt; 150; $x++) {'],
             ["'namespace ';", "'<span class=\"hl-value\">namespace </span>';"],
+            ['$class', '$class'],
             [
                 "// We'll
 

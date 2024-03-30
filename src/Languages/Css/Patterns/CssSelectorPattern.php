fadb27da69f4e014d7114f7318d4b5af938b1e0b
brendt <brent.roose@gmail.com>
2024-03-30 06:19:59 +0100

    Improve CSS Selector pattern

--- src/Languages/Css/Patterns/CssSelectorPattern.php
+++ src/Languages/Css/Patterns/CssSelectorPattern.php
@@ -22,13 +22,15 @@ kbd, samp,
 pre ',
 )]
 #[PatternTest(input: "[data-x='asd'] .light {", output: "[data-x='asd'] .light ")]
+#[PatternTest(input: "foo + bar {", output: "foo + bar ")]
+#[PatternTest(input: "foo:hover {", output: "foo:hover ")]
 final readonly class CssSelectorPattern implements Pattern
 {
     use IsPattern;
 
     public function getPattern(): string
     {
-        return '(?<match>[\[\]\'\"\=\@\-\#\.\w\s,\n]+)\{';
+        return '(?<match>[\[\]\'\"\=\@\-\#\.\w\s,\n\+\:]+)\{';
     }
 
     public function getTokenType(): TokenTypeEnum

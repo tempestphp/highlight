97ec0d6631d0e3215772f0ce830eff06858d5997
Nima HeydariNasab <nima.heydari79@yahoo.com>
2024-08-28 19:39:55 +0330

    Fix for gutter tests

--- tests/Languages/Base/Injections/GutterInjectionTest.php
+++ tests/Languages/Base/Injections/GutterInjectionTest.php
@@ -32,22 +32,22 @@ foreach ($lines as $i => $line) {
 TXT;
 
         $expected = <<<'TXT'
-<span class="hl-gutter">  10</span> <span class="hl-keyword">foreach</span> (<span class="hl-variable">$lines</span> <span class="hl-keyword">as</span> <span class="hl-variable">$i</span> =&gt; <span class="hl-variable">$line</span>) {
-<span class="hl-gutter">  11</span>     <span class="hl-variable">$gutterNumber</span> = <span class="hl-variable">$gutterNumbers</span>[<span class="hl-variable">$i</span>];
-<span class="hl-gutter">  12</span> 
-<span class="hl-gutter">  13</span>     <span class="hl-variable">$gutterClass</span> = '<span class="hl-value">hl-gutter </span>' . (<span class="hl-variable">$this</span>-&gt;<span class="hl-property">classes</span>[<span class="hl-variable">$i</span> + 1] ?? '<span class="hl-value"></span>');
-<span class="hl-gutter hl-gutter-addition">14 +</span> <span class="hl-addition"></span>
-<span class="hl-gutter hl-gutter-addition">15 +</span> <span class="hl-addition">    <span class="hl-variable">$lines</span>[<span class="hl-variable">$i</span>] = <span class="hl-property">sprintf</span>(</span>
-<span class="hl-gutter hl-gutter-addition">16 +</span> <span class="hl-addition">        <span class="hl-type">Escape</span>::<span class="hl-property">tokens</span>('<span class="hl-value">&lt;span class=&quot;%s&quot;&gt;%s&lt;/span&gt;%s</span>'),</span>
-<span class="hl-gutter">  17</span>         <span class="hl-variable">$gutterClass</span>,
-<span class="hl-gutter">  18</span>         <span class="hl-property">str_pad</span>(
-<span class="hl-gutter hl-gutter-deletion">19 -</span>             <span class="hl-property">string</span>: <span class="hl-deletion"><span class="hl-variable">$gutterNumber</span></span>,
-<span class="hl-gutter">  20</span>             <span class="hl-property">length</span>: <span class="hl-variable">$gutterWidth</span>,
-<span class="hl-gutter">  21</span>             <span class="hl-property">pad_type</span>: <span class="hl-property">STR_PAD_LEFT</span>,
-<span class="hl-gutter">  22</span>         ),
-<span class="hl-gutter">  23</span>         <span class="hl-variable">$line</span>,
-<span class="hl-gutter">  24</span>     );
-<span class="hl-gutter">  25</span> }
+<span class="hl-gutter">  10</span><span class="hl-keyword">foreach</span> (<span class="hl-variable">$lines</span> <span class="hl-keyword">as</span> <span class="hl-variable">$i</span> =&gt; <span class="hl-variable">$line</span>) {
+<span class="hl-gutter">  11</span>    <span class="hl-variable">$gutterNumber</span> = <span class="hl-variable">$gutterNumbers</span>[<span class="hl-variable">$i</span>];
+<span class="hl-gutter">  12</span>
+<span class="hl-gutter">  13</span>    <span class="hl-variable">$gutterClass</span> = '<span class="hl-value">hl-gutter </span>' . (<span class="hl-variable">$this</span>-&gt;<span class="hl-property">classes</span>[<span class="hl-variable">$i</span> + 1] ?? '<span class="hl-value"></span>');
+<span class="hl-gutter hl-gutter-addition">14 +</span><span class="hl-addition"></span>
+<span class="hl-gutter hl-gutter-addition">15 +</span><span class="hl-addition">    <span class="hl-variable">$lines</span>[<span class="hl-variable">$i</span>] = <span class="hl-property">sprintf</span>(</span>
+<span class="hl-gutter hl-gutter-addition">16 +</span><span class="hl-addition">        <span class="hl-type">Escape</span>::<span class="hl-property">tokens</span>('<span class="hl-value">&lt;span class=&quot;%s&quot;&gt;%s&lt;/span&gt;%s</span>'),</span>
+<span class="hl-gutter">  17</span>        <span class="hl-variable">$gutterClass</span>,
+<span class="hl-gutter">  18</span>        <span class="hl-property">str_pad</span>(
+<span class="hl-gutter hl-gutter-deletion">19 -</span>            <span class="hl-property">string</span>: <span class="hl-deletion"><span class="hl-variable">$gutterNumber</span></span>,
+<span class="hl-gutter">  20</span>            <span class="hl-property">length</span>: <span class="hl-variable">$gutterWidth</span>,
+<span class="hl-gutter">  21</span>            <span class="hl-property">pad_type</span>: <span class="hl-property">STR_PAD_LEFT</span>,
+<span class="hl-gutter">  22</span>        ),
+<span class="hl-gutter">  23</span>        <span class="hl-variable">$line</span>,
+<span class="hl-gutter">  24</span>    );
+<span class="hl-gutter">  25</span>}
 TXT;
         $highlighter = (new Highlighter())->withGutter(10);
 
@@ -65,11 +65,11 @@ other: foo
 TXT;
 
         $expected = <<<'TXT'
-<span class="hl-gutter">  10</span> <span class="hl-property">on</span>:
-<span class="hl-gutter">  11</span>   <span class="hl-property">pull_request</span>:
-<span class="hl-gutter">  12</span>     <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review]
-<span class="hl-gutter hl-gutter-addition">13 +</span> <span class="hl-addition"> <span class="hl-property">pull_request_target</span>: </span>
-<span class="hl-gutter">  14</span> <span class="hl-property">other</span>: foo
+<span class="hl-gutter">  10</span><span class="hl-property">on</span>:
+<span class="hl-gutter">  11</span>  <span class="hl-property">pull_request</span>:
+<span class="hl-gutter">  12</span>    <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review]
+<span class="hl-gutter hl-gutter-addition">13 +</span><span class="hl-addition"> <span class="hl-property">pull_request_target</span>: </span>
+<span class="hl-gutter">  14</span><span class="hl-property">other</span>: foo
 TXT;
 
         $highlighter = (new Highlighter())->withGutter(10);
@@ -91,14 +91,14 @@ other: foo
 TXT;
 
         $expected = <<<'TXT'
-<span class="hl-gutter">  10</span> <span class="hl-property">on</span>:
-<span class="hl-gutter">  11</span>   <span class="hl-property">pull_request</span>:
-<span class="hl-gutter">  12</span>     <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review]
-<span class="hl-gutter hl-gutter-addition">13 +</span> <span class="hl-addition"> <span class="hl-property">pull_request_target</span>: </span>
-<span class="hl-gutter hl-gutter-addition">14 +</span> <span class="hl-addition">   <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review] </span>
-<span class="hl-gutter hl-gutter-addition">15 +</span> <span class="hl-addition">   <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review] </span>
-<span class="hl-gutter hl-gutter-addition">16 +</span> <span class="hl-addition">   <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review] </span>
-<span class="hl-gutter">  17</span> <span class="hl-property">other</span>: foo
+<span class="hl-gutter">  10</span><span class="hl-property">on</span>:
+<span class="hl-gutter">  11</span>  <span class="hl-property">pull_request</span>:
+<span class="hl-gutter">  12</span>    <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review]
+<span class="hl-gutter hl-gutter-addition">13 +</span><span class="hl-addition"> <span class="hl-property">pull_request_target</span>: </span>
+<span class="hl-gutter hl-gutter-addition">14 +</span><span class="hl-addition">   <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review] </span>
+<span class="hl-gutter hl-gutter-addition">15 +</span><span class="hl-addition">   <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review] </span>
+<span class="hl-gutter hl-gutter-addition">16 +</span><span class="hl-addition">   <span class="hl-property">types</span>: [opened, synchronize, reopened, ready_for_review] </span>
+<span class="hl-gutter">  17</span><span class="hl-property">other</span>: foo
 TXT;
 
         $highlighter = (new Highlighter())->withGutter(10);

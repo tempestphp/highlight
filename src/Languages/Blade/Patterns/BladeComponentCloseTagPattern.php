3af0a70123c7c109b5c3ee27348d90fe4b13733b
Tony Messias <tonysm@hey.com>
2024-10-20 01:20:06 -0300

    Support dotted notation on namespaced components

--- src/Languages/Blade/Patterns/BladeComponentCloseTagPattern.php
+++ src/Languages/Blade/Patterns/BladeComponentCloseTagPattern.php
@@ -9,16 +9,17 @@ use Tempest\Highlight\Pattern;
 use Tempest\Highlight\PatternTest;
 use Tempest\Highlight\Tokens\TokenTypeEnum;
 
+#[PatternTest(input: '</a>', output: 'a')]
 #[PatternTest(input: '</x-hello>', output: 'x-hello')]
 #[PatternTest(input: '</x-hello::world>', output: 'x-hello::world')]
-#[PatternTest(input: '</a>', output: 'a')]
+#[PatternTest(input: '</x-hello::world.lorem>', output: 'x-hello::world.lorem')]
 final readonly class BladeComponentCloseTagPattern implements Pattern
 {
     use IsPattern;
 
     public function getPattern(): string
     {
-        return '<\/(?<match>[\w\-\:]+)';
+        return '<\/(?<match>[\w\-\:\.]+)';
     }
 
     public function getTokenType(): TokenTypeEnum
--- src/Languages/Blade/Patterns/BladeComponentOpenTagPattern.php
+++ src/Languages/Blade/Patterns/BladeComponentOpenTagPattern.php
@@ -9,16 +9,17 @@ use Tempest\Highlight\Pattern;
 use Tempest\Highlight\PatternTest;
 use Tempest\Highlight\Tokens\TokenTypeEnum;
 
+#[PatternTest(input: '<a attr="">', output: 'a')]
 #[PatternTest(input: '<x-hello attr="">', output: 'x-hello')]
 #[PatternTest(input: '<x-hello::world attr="">', output: 'x-hello::world')]
-#[PatternTest(input: '<a attr="">', output: 'a')]
+#[PatternTest(input: '<x-hello::world.lorem attr="">', output: 'x-hello::world.lorem')]
 final readonly class BladeComponentOpenTagPattern implements Pattern
 {
     use IsPattern;
 
     public function getPattern(): string
     {
-        return '<(?<match>[\w\-\:]+)';
+        return '<(?<match>[\w\-\:\.]+)';
     }
 
     public function getTokenType(): TokenTypeEnum

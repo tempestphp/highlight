da76f7034c0398531a3742c738541b73386ad086
brendt <brent.roose@gmail.com>
2025-03-06 10:52:08 +0100

    wip

--- src/Languages/Blade/BladeLanguage.php
+++ src/Languages/Blade/BladeLanguage.php
@@ -4,42 +4,6 @@ declare(strict_types=1);
 
 namespace Tempest\Highlight\Languages\Blade;
 
-use Tempest\Highlight\Languages\Blade\Injections\BladeEchoInjection;
-use Tempest\Highlight\Languages\Blade\Injections\BladeKeywordInjection;
-use Tempest\Highlight\Languages\Blade\Injections\BladePhpInjection;
-use Tempest\Highlight\Languages\Blade\Injections\BladeRawEchoInjection;
-use Tempest\Highlight\Languages\Blade\Patterns\BladeCommentPattern;
-use Tempest\Highlight\Languages\Blade\Patterns\BladeComponentCloseTagPattern;
-use Tempest\Highlight\Languages\Blade\Patterns\BladeComponentOpenTagPattern;
-use Tempest\Highlight\Languages\Blade\Patterns\BladeKeywordPattern;
-use Tempest\Highlight\Languages\Html\HtmlLanguage;
-
-class BladeLanguage extends HtmlLanguage
+class BladeLanguage
 {
-    public function getName(): string
-    {
-        return 'blade';
-    }
-
-    public function getInjections(): array
-    {
-        return [
-            ...parent::getInjections(),
-            new BladeKeywordInjection(),
-            new BladePhpInjection(),
-            new BladeEchoInjection(),
-            new BladeRawEchoInjection(),
-        ];
-    }
-
-    public function getPatterns(): array
-    {
-        return [
-            ...parent::getPatterns(),
-            new BladeComponentOpenTagPattern(),
-            new BladeComponentCloseTagPattern(),
-            new BladeKeywordPattern(),
-            new BladeCommentPattern(),
-        ];
-    }
 }
--- tests/targets/test.md
+++ tests/targets/test.md
@@ -1,5 +1,9 @@
-```php
-public function __construct(
-    private(set) Author $author,
-) {}
+```blade
+{{-- The post's title --}}
+
+@if($post->title)
+    <h1>{{ $post->title }}</h1>  
+@else
+    <span>-</span>
+@endif
 ```
